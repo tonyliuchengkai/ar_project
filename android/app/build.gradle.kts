f0b6b99f4a554d9680a203e1cffad6b9e5f66170
tonyliuchengkai <tonyt712kd@gmail.com>
2025-04-09 20:31:45 +0800

    add

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
 android {
     namespace = "com.example.local_and_web_objects"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    //ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
